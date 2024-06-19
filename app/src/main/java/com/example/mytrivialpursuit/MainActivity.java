4c5799c5607db8777906281feac856de159ea2e2
Jules02 <julesdupont02@gmail.com>
2024-06-19 17:39:58 +0200

    End of step 2

--- app/src/main/java/com/example/mytrivialpursuit/MainActivity.java
+++ app/src/main/java/com/example/mytrivialpursuit/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.mytrivialpursuit;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -10,9 +12,33 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
+import java.util.Vector;
+import java.util.stream.IntStream;
 
-    int ANSWER = 3;
+public class MainActivity extends AppCompatActivity {
+    Random r;
+
+    int i_right;
+
+    Quiz q;
+    int ind_c;
+    Carte c;
+
+
+    int n;
+    int[] cartes_restantes;
+
+    public Button pick_button(int index) {
+        if (index == 1) {
+            return findViewById(R.id.button1);
+        } else if (index == 2) {
+            return findViewById(R.id.button2);
+        } else if (index == 3) {
+            return findViewById(R.id.button3);
+        }
+        return findViewById(R.id.button4);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +50,43 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        r = new Random();
+
+        q = new Quiz();
+
+        ind_c = 0;
+
+        newCarte();
+
+        //n = q.getNbCartes();
+        //cartes_restantes = IntStream.range(1, n+1).toArray();
+    }
+
+    public void newCarte() {
+        c = q.getCarte(ind_c);
+
+        TextView question_text = findViewById(R.id.textView_question);
+        question_text.setText(c.getQuestion());
+
+        i_right = r.nextInt(4) + 1;
+        Button b_right = pick_button(i_right);
+        b_right.setText(c.getBonneReponse());
+
+        Vector<String> wrong_ans = c.getMauvaisesReponses();
+        int k = 0;
+        for (int i = 0; i < 4; i++) {
+            if (i != i_right) {
+                Button b_wrong = pick_button(i);
+                b_wrong.setText(wrong_ans.get(k));
+                k++;
+            }
+        }
     }
 
     public void reponse(View view) {
         int ans;
-        if (view.getId() == R.id.button) {
+        if (view.getId() == R.id.button1) {
             ans = 1;
         } else if (view.getId() == R.id.button2) {
             ans = 2;
@@ -39,8 +97,14 @@ public class MainActivity extends AppCompatActivity {
         }
 
         Toast toast = new Toast(getApplicationContext());
-        if (ans == ANSWER) {
+        if (ans == i_right) {
             toast.setText("Bravo !");
+            ind_c++;
+            if (ind_c < 3) {
+                newCarte();
+            } else {
+                toast.setText("Fin du test, félicitations !");
+            }
         } else {
             toast.setText("Mauvaise réponse.");
         }
